18086217cb76ced3a07328133eebf9e974ec54e6
Melvin Schulten <Melvin.Schulten@CAE.de>
2019-12-16 15:45:06 +0100

    A: day06 (really not proud on this one)

--- src/main/kotlin/io/github/melvinsc/utils/collections.kt
+++ src/main/kotlin/io/github/melvinsc/utils/collections.kt
@@ -1,6 +1,8 @@
 package io.github.melvinsc.utils
 
 import java.util.*
+import kotlin.collections.HashMap
+import kotlin.collections.HashSet
 
 fun <T> Iterable<T>.getGroups(): List<List<T>> {
     val ret = mutableListOf<MutableList<T>>()
@@ -21,4 +23,111 @@ fun <T> linkedListOf(vararg items: T) = LinkedList<T>().apply {
     for (i in items) {
         add(i)
     }
+}
+
+class BufferTree<T> {
+    private var root: Node<T>? = null
+    private val orphans = mutableListOf<Pair<T, T>>()
+
+    fun insert(element: T, child: T, isRoot: Boolean) {
+        if (isRoot) {
+            setRoot(element, child)
+        } else {
+            if (putToParent(element, child) && !isValid()) {
+                tryReinsert()
+            } else {
+                orphans.add(Pair(element, child))
+            }
+        }
+    }
+
+    fun isValid(): Boolean = orphans.isEmpty()
+
+    fun setRoot(element: T, child: T) {
+        root = Node(element, child)
+        tryReinsert()
+    }
+
+    private fun putToParent(element: T, child: T): Boolean {
+        val parentNode = getNode(element)
+
+        if (parentNode != null) {
+            parentNode.addChild(Node(child))
+            return true
+        }
+
+        return false
+    }
+
+    fun getNode(element: T): Node<T>? {
+        if (root?.content == element) return root
+
+        val nodes = root?.getChilds()?.toMutableList()
+
+        if (nodes != null) {
+            while (nodes.isNotEmpty()) {
+                val current = nodes.first()
+
+                if (current.content?.equals(element) == true) {
+                    return current
+                } else {
+                    nodes.addAll(current.getChilds())
+                    nodes.removeAt(0)
+                }
+            }
+        }
+
+        return null
+    }
+
+    private fun tryReinsert() {
+        var changed = false
+        var current: Pair<T, T>
+        var i = 0
+
+        while (i in orphans.indices) {
+            current = orphans[i]
+
+            if (putToParent(current.first, current.second)) {
+                orphans.removeAt(i)
+                changed = true
+            } else {
+                ++i
+            }
+        }
+
+        if (changed) tryReinsert()
+    }
+
+    class Node<U>(val content: U, child: U? = null) {
+        private val children = LinkedList<Node<U>>()
+
+        init {
+            if (child != null) {
+                children.add(Node(child))
+            }
+        }
+
+        fun addChild(child: Node<U>) = children.add(child)
+
+        fun getChilds(): List<Node<U>> = children.toList()
+    }
+}
+
+class Graph<T> {
+    private val adjacencyMap: HashMap<T, HashSet<T>> = HashMap()
+
+    fun addEdge (sourceVertex: T, destinationVertex: T) {
+        adjacencyMap.computeIfAbsent(sourceVertex) { HashSet() }.add(destinationVertex)
+        adjacencyMap.computeIfAbsent(destinationVertex) { HashSet() }.add(sourceVertex)
+    }
+
+    operator fun get (sourceVertex: T) : HashSet<T>? = adjacencyMap[sourceVertex]
+
+    override fun toString(): String = StringBuffer().apply {
+        for (key in adjacencyMap.keys) {
+            append("$key -> ")
+            append(adjacencyMap[key]?.joinToString(",", "[", "]\n"))
+        }
+    }.toString()
 }
---
+++ src/main/kotlin/io/github/melvinsc/year2019/day06/main.kt
@@ -0,0 +1,72 @@
+package io.github.melvinsc.year2019.day06
+
+import io.github.melvinsc.utils.Graph
+import io.github.melvinsc.utils.BufferTree
+import io.github.melvinsc.utils.day.Day
+
+fun main() = Day.setMain(Day06)
+
+object Day06 : Day() {
+    override fun first(input: String): Int {
+        val tree = input.split("\n")
+            .map { line -> line.split(")") }
+            .fold(BufferTree<String>()) { acc, orbit ->
+                acc.insert(orbit[0], orbit[1], orbit[0] == "COM")
+                return@fold acc
+            }
+
+        var sum = 0
+        val list = mutableListOf<Pair<BufferTree.Node<String>?, Int>>()
+        list.add(Pair(tree.getNode("COM"), 0))
+        if (tree.isValid()) {
+            while (list.isNotEmpty()) {
+                val current = list.first().also { list.removeAt(0) }
+                sum += current.second
+
+                for (element in current.first?.getChilds() ?: throw IllegalStateException("Something is fucked up")) {
+                    list.add(Pair(element, current.second + 1))
+                }
+            }
+
+            return sum
+        } else {
+            throw IllegalStateException("The Tree is in an Illegal State")
+        }
+    }
+
+    override fun second(input: String): Int {
+        val graph = input.split("\n")
+            .map { line -> line.split(")") }
+            .fold(Graph<String>()) { acc, orbit ->
+                acc.addEdge(orbit[0], orbit[1])
+                return@fold acc
+            }
+
+        val youPlanet = graph["YOU"]?.asSequence()?.first()
+        val sanPlanet = graph["SAN"]?.asSequence()?.first()
+
+        if (youPlanet != null && sanPlanet != null) {
+            val map = HashMap<String, Int>()
+            val setToAdd = HashSet<String>().apply { add(youPlanet) }
+            val lastSet = HashSet<String>()
+            var currentDepth = 0
+
+            while (!map.contains(sanPlanet)) {
+                setToAdd.forEach { map.putIfAbsent(it, currentDepth) }
+                lastSet.addAll(setToAdd)
+                setToAdd.clear()
+
+                lastSet.forEach {
+                    val tmp = graph[it] ?: throw IllegalStateException("Something is fucked up")
+                    setToAdd.addAll(tmp.map { s -> s })
+                }
+
+                ++currentDepth
+            }
+
+            return currentDepth -1
+        } else {
+            throw IllegalStateException("You no have Santa or yourself:(")
+        }
+    }
+}
