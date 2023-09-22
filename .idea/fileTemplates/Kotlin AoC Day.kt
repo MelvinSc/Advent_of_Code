11388695086441815c2b45973cbf60e98a7355c4
Melvin Schulten <melvinschulten@gmail.com>
2023-09-22 14:41:52 +0200

    improved template for Day.kt

--- .idea/fileTemplates/Kotlin AoC Day.kt
+++ .idea/fileTemplates/Kotlin AoC Day.kt
@@ -1,5 +1,5 @@
-#set($date=${PACKAGE_NAME})
-#set($date=$date.substring(31, 33))
+#set($packageName=${PACKAGE_NAME})
+#set($date=$packageName.substring(31, 33))
 package ${PACKAGE_NAME}
 
 import io.github.melvinsc.utils.day.Day
@@ -8,12 +8,10 @@ fun main() = Day.setMain(Day$date)
 
 object Day$date : Day() {
     override fun first(inputData: String): Int {
-
         return -1
     }
 
     override fun second(inputData: String): Int {
-
         return -1
     }
 }
